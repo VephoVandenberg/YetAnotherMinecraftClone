0746bb0f101759cf811a0899728bfa1bee87a5d2
VephoVandenberg <azamatthebest.cs.py@gmail.com>
2023-04-09 18:55:03 +0300

    Now we've got cubemaps

--- src/game_modules/block/block.cpp
+++ src/game_modules/block/block.cpp
@@ -11,43 +11,43 @@ constexpr unsigned int g_numberOfFaces = 36;
 Block::Block(glm::vec3 pos, BlockType type)
 	: m_pos(pos)
 	, m_type(type)
-{	
+{
 	m_vertices = {
 		// front
 		{ m_pos + glm::vec3(-0.5f, -0.5f,  0.5f), {-0.5f, -0.5f,  0.5f} },
-		{ m_pos + glm::vec3(0.5f, -0.5f,  0.5f), { 0.5f, -0.5f,  0.5f} },
+		{ m_pos + glm::vec3( 0.5f, -0.5f,  0.5f), { 0.5f, -0.5f,  0.5f} },
 		{ m_pos + glm::vec3(-0.5f,  0.5f,  0.5f), {-0.5f,  0.5f,  0.5f} },
-		{ m_pos + glm::vec3(0.5f,  0.5f,  0.5f), { 0.5f,  0.5f,  0.5f} },
-		
+		{ m_pos + glm::vec3( 0.5f,  0.5f,  0.5f), { 0.5f,  0.5f,  0.5f} },
+
 		// back
 		{ m_pos + glm::vec3(-0.5f, -0.5f, -0.5f), { -0.5f, -0.5f, -0.5f} },
-		{ m_pos + glm::vec3(0.5f, -0.5f, -0.5f), {  0.5f, -0.5f, -0.5f} },
+		{ m_pos + glm::vec3( 0.5f, -0.5f, -0.5f), {  0.5f, -0.5f, -0.5f} },
 		{ m_pos + glm::vec3(-0.5f,  0.5f, -0.5f), { -0.5f,  0.5f, -0.5f} },
-		{ m_pos + glm::vec3(0.5f,  0.5f, -0.5f), {  0.5f,  0.5f, -0.5f} },
-		
+		{ m_pos + glm::vec3( 0.5f,  0.5f, -0.5f), {  0.5f,  0.5f, -0.5f} },
+
 		// top
-		{ m_pos + glm::vec3(-0.5f,  0.5f,  0.5f), glm::vec3(-0.5f,  0.5f,  0.5f) },
-		{ m_pos + glm::vec3( 0.5f,  0.5f,  0.5f), glm::vec3( 0.5f,  0.5f,  0.5f) },
-		{ m_pos + glm::vec3(-0.5f,  0.5f, -0.5f), glm::vec3(-0.5f,  0.5f, -0.5f) },
-		{ m_pos + glm::vec3( 0.5f,  0.5f, -0.5f), glm::vec3( 0.5f,  0.5f, -0.5f) },
-																		
+		{ m_pos + glm::vec3(-0.5f,  0.5f,  0.5f), {-0.5f,  0.5f,  0.5f} },
+		{ m_pos + glm::vec3( 0.5f,  0.5f,  0.5f), { 0.5f,  0.5f,  0.5f} },
+		{ m_pos + glm::vec3(-0.5f,  0.5f, -0.5f), {-0.5f,  0.5f, -0.5f} },
+		{ m_pos + glm::vec3( 0.5f,  0.5f, -0.5f), { 0.5f,  0.5f, -0.5f} },
+
 		// bottom
-		{ m_pos + glm::vec3(-0.5f, -0.5f,  0.5f), glm::vec3(-0.5f, -0.5f,  0.5f) },
-		{ m_pos + glm::vec3(0.5f,  -0.5f,  0.5f), glm::vec3( 0.5f, -0.5f,  0.5f) },
-		{ m_pos + glm::vec3(-0.5f, -0.5f, -0.5f), glm::vec3(-0.5f, -0.5f, -0.5f) },
-		{ m_pos + glm::vec3(0.5f,  -0.5f, -0.5f), glm::vec3( 0.5f, -0.5f, -0.5f) },
-		
+		{ m_pos + glm::vec3(-0.5f, -0.5f,  0.5f), {-0.5f, -0.5f,  0.5f } },
+		{ m_pos + glm::vec3(0.5f,  -0.5f,  0.5f), { 0.5f, -0.5f,  0.5f } },
+		{ m_pos + glm::vec3(-0.5f, -0.5f, -0.5f), {-0.5f, -0.5f, -0.5f } },
+		{ m_pos + glm::vec3(0.5f,  -0.5f, -0.5f), { 0.5f, -0.5f, -0.5f } },
+
 		// left
-		{ m_pos + glm::vec3(-0.5f, -0.5f, -0.5f), glm::vec3(-0.5f, -0.5f, -0.5f) },
-		{ m_pos + glm::vec3(-0.5f, -0.5f,  0.5f), glm::vec3(-0.5f, -0.5f,  0.5f) },
-		{ m_pos + glm::vec3(-0.5f,  0.5f, -0.5f), glm::vec3(-0.5f,  0.5f, -0.5f) },
-		{ m_pos + glm::vec3(-0.5f,  0.5f,  0.5f), glm::vec3(-0.5f,  0.5f,  0.5f) },
-		
+		{ m_pos + glm::vec3(-0.5f, -0.5f, -0.5f), {-0.5f, -0.5f, -0.5f } },
+		{ m_pos + glm::vec3(-0.5f, -0.5f,  0.5f), {-0.5f, -0.5f,  0.5f } },
+		{ m_pos + glm::vec3(-0.5f,  0.5f, -0.5f), {-0.5f,  0.5f, -0.5f } },
+		{ m_pos + glm::vec3(-0.5f,  0.5f,  0.5f), {-0.5f,  0.5f,  0.5f } },
+
 		// right
-		{ m_pos + glm::vec3(0.5f, -0.5f, -0.5f), glm::vec3(0.5f, -0.5f, -0.5f)  },
-		{ m_pos + glm::vec3(0.5f, -0.5f,  0.5f), glm::vec3(0.5f, -0.5f,  0.5f)  },
-		{ m_pos + glm::vec3(0.5f,  0.5f, -0.5f), glm::vec3(0.5f,  0.5f, -0.5f)  },
-		{ m_pos + glm::vec3(0.5f,  0.5f,  0.5f), glm::vec3(0.5f,  0.5f,  0.5f)  }
+		{ m_pos + glm::vec3(0.5f, -0.5f, -0.5f), {0.5f, -0.5f, -0.5f } },
+		{ m_pos + glm::vec3(0.5f, -0.5f,  0.5f), {0.5f, -0.5f,  0.5f } },
+		{ m_pos + glm::vec3(0.5f,  0.5f, -0.5f), {0.5f,  0.5f, -0.5f } },
+		{ m_pos + glm::vec3(0.5f,  0.5f,  0.5f), {0.5f,  0.5f,  0.5f } }
 	};
 
 	switch (m_type)
